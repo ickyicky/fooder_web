b996edd9e3a6657670dd7d83aa489a4defe42d80
Cyanose <KrzysztofPastuszka@protonmail.com>
2023-07-29 19:46:11 +0200

    radom jazda

---
+++ lib/models/product.dart
@@ -0,0 +1,18 @@
+class Product {
+  final int id;
+  final String name;
+  final double calories;
+  final double protein;
+  final double carb;
+  final double fat;
+
+
+  const Entry({
+    required this.id,
+    required this.name;
+    required this.calories;
+    required this.protein;
+    required this.carb;
+    required this.fat;
+  });
+}
